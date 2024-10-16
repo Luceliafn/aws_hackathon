d060c10499ddc7b1cce193dcb2795d91c95ca5b5
danielpcinalli <32197292+danielpcinalli@users.noreply.github.com>
2024-10-16 15:33:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ logger = logging.getLogger(__name__)
 
 REGION = 'us-west-2'
 
-
-
 if "messages" not in st.session_state:
     st.session_state['messages'] = []
 
@@ -21,22 +19,6 @@ if 'agent_client' not in st.session_state:
 if 'session_id' not in st.session_state:
     st.session_state['session_id'] = str(uuid.uuid1())
 
-if 'agents' not in st.session_state or True:
-    bedrock_agent = boto3.client('bedrock-agent', region_name=REGION)
-
-    agents = {
-        agent['agentName']: {
-            'agentId': agent['agentId'], 
-            'agentAliases': dict()
-        } for agent in bedrock_agent.list_agents()['agentSummaries']
-    }
-    for name, info in agents.items():
-        for alias in bedrock_agent.list_agent_aliases(agentId=info['agentId'])['agentAliasSummaries']:
-            info['agentAliases'][alias['agentAliasName']] = alias['agentAliasId']
-
-
-    st.session_state['agents'] = agents
-
 # escolha fixa de agente e alias
 st.session_state['agentId'] = 'OIA2DP7SBI'
 st.session_state['aliasId'] = 'UFEVWBMLEC'
@@ -44,28 +26,8 @@ st.session_state['aliasId'] = 'UFEVWBMLEC'
 
 def gera_resposta_agente(input_text):
 
-    session_state = {
-        'knowledgeBaseConfigurations': [
-            {
-                'knowledgeBaseId': "L4MSFKO2YY",
-                'retrievalConfiguration': {
-                    "vectorSearchConfiguration": {
-                        "numberOfResults": 20,
-                        "overrideSearchType": "HYBRID",
-                        "filter": {
-                            "equals": {
-                                "key": "num_processo",
-                                "value": str(st.session_state['num_processo'])
-                            }
-                        }
-                    }
-                }
-            }
-        ]
-    }
-
-    enable_trace:bool = False
-    end_session:bool = False
+    enable_trace = False
+    end_session = False
 
     # invoke the agent API
     agentResponse = st.session_state['agent_client'].invoke_agent(
@@ -75,7 +37,6 @@ def gera_resposta_agente(input_text):
         sessionId=st.session_state['session_id'],
         enableTrace=enable_trace, 
         endSession= end_session,
-        sessionState= session_state
     )
     event_stream = agentResponse['completion']
     try:
@@ -91,27 +52,16 @@ def gera_resposta_agente(input_text):
     except Exception as e:
         raise Exception("unexpected event.", e)
 
-
-# Seletores de agente e alias
-# col_agent, col_alias = st.columns(2)
-# agent = col_agent.selectbox('Agent', st.session_state['agents'].keys())
-# alias = col_alias.selectbox('Alias', st.session_state['agents'][agent]['agentAliases'].keys())
-
-# guarda escolha
-# st.session_state['agentId'] = st.session_state['agents'][agent]['agentId']
-# st.session_state['aliasId'] = st.session_state['agents'][agent]['agentAliases'][alias]
-
-col_num_processo, col_botao_atualizar = st.columns(2)
-
-num_processo = col_num_processo.text_input(label='Número do processo')
-if col_botao_atualizar.button('Atualizar processo'):
-    st.session_state['num_processo'] = num_processo
+if st.button('Começar nova sessão'):
     st.session_state['messages'] = []
+    st.session_state['session_id'] = str(uuid.uuid1())
+
 
 # escreve histórico de mensagens
+# chat_container = st.container(height=700)
 for message in st.session_state['messages']:
     with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+        st.markdown(message["content"].replace('$', '\$'))
 
 # caixa de prompt
 prompt = st.chat_input("Insira suas perguntas sobre um processo")
@@ -130,35 +80,3 @@ if prompt:
         stream = gera_resposta_agente(message)
         response = st.write_stream(stream)
     st.session_state.messages.append({"role": "assistant", "content": response})
-
-# 07001777820248070011
-# liste os envolvidos
-# quem é a autora?
-# 07097029720238070018
-# liste os envolvidos no processo 07001777820248070011
-# liste os envolvidos no processo 07097029720238070018
-# liste os envolvidos no processo 07430185520238070001
-
-# liste os envolvidos no processo 07206392320238070001
-# liste os envolvidos no processo 07251462720238070001
-# liste os envolvidos no processo 07471652720238070001
-# liste os envolvidos no processo 10023333420178260123
-# liste os envolvidos no processo 10075424420208260554
-# faça um resumo do processo 07251462720238070001
-# faça um resumo do processo 07206392320238070001
-# faça um resumo do processo 07471652720238070001
-# faça um resumo do processo 10023333420178260123
-# faça um resumo do processo 10075424420208260554
-
-
-
-
-
-# script - vídeo
-
-
-# Faça um resumo da sentença do processo 07471652720238070001
-# Qual o valor do débito gerado pela autora neste processo?
-
-
-# Qual o valor do débito gerado pela autora no processo 07471652720238070001?
